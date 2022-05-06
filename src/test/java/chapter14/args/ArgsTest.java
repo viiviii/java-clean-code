df997bc164a955325f16d327c511730eb0c78e28
viiviii <viiiix.viiviii@gmail.com>
2022-05-06 15:49:48 +0900

    test: String 인수 유형 추가 버전 테스트 추가

--- src/test/java/chapter14/args/ArgsTest.java
+++ src/test/java/chapter14/args/ArgsTest.java
@@ -3,13 +3,16 @@ package chapter14.args;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
 
 class ArgsTest {
 
     @DisplayName("scheme와 arguments가 없는 경우")
     @Test
-    void noSchemaAndArguments() {
+    void noSchemaAndArguments() throws Exception {
         //given
         String schema = "";
         String[] arguments = new String[0];
@@ -24,7 +27,7 @@ class ArgsTest {
 
     @DisplayName("scheme가 없고 argument가 1개 있는 경우")
     @Test
-    void noSchemaButWithOneArgument() {
+    void noSchemaButWithOneArgument() throws Exception {
         //given
         String schema = "";
         String[] arguments = new String[]{"-x"};
@@ -40,7 +43,7 @@ class ArgsTest {
 
     @DisplayName("scheme가 없고 argument가 여러 개 있는 경우")
     @Test
-    void noSchemaButWithMultipleArguments() {
+    void noSchemaButWithMultipleArguments() throws Exception {
         //given
         String schema = "";
         String[] arguments = new String[]{"-x", "-y"};
@@ -56,7 +59,7 @@ class ArgsTest {
 
     @DisplayName("boolean 값이 없는 경우")
     @Test
-    void simpleBooleanNotPresent() {
+    void simpleBooleanNotPresent() throws Exception {
         //given
         String schema = "x";
         String[] arguments = new String[]{};
@@ -72,7 +75,7 @@ class ArgsTest {
 
     @DisplayName("boolean 값이 있는 경우")
     @Test
-    void simpleBooleanPresent() {
+    void simpleBooleanPresent() throws Exception {
         //given
         String schema = "x";
         String[] arguments = new String[]{"-x"};
@@ -88,9 +91,9 @@ class ArgsTest {
 
     @DisplayName("boolean 값이 여러 개 있는 경우")
     @Test
-    void simpleBooleanMultiplePresent() {
+    void simpleBooleanMultiplePresent() throws Exception {
         //given
-        String schema = "x,y";
+        String schema = "x,y,z";
         String[] arguments = new String[]{"-x", "-y"};
 
         //when
@@ -99,16 +102,37 @@ class ArgsTest {
         //then
         assertThat(args.isValid()).isTrue();
         assertThat(args.cardinality()).isEqualTo(2);
+        assertThat(args.has('x')).isTrue();
+        assertThat(args.has('y')).isTrue();
+        assertThat(args.has('z')).isFalse();
         assertThat(args.getBoolean('x')).isTrue();
         assertThat(args.getBoolean('y')).isTrue();
+        assertThat(args.getBoolean('z')).isFalse();
     }
 
-    @DisplayName("boolean 값이 여러 개 중 하나만 있는 경우")
+    @DisplayName("String 값이 없는 경우")
     @Test
-    void simpleBooleanOnlyOnePresent() {
+    void simpleStringNotPresent() throws Exception {
         //given
-        String schema = "x,y,z";
-        String[] arguments = new String[]{"-z"};
+        String schema = "x*";
+        String[] arguments = new String[]{};
+
+        //when
+        Args args = new Args(schema, arguments);
+
+        //then
+        assertThat(args.isValid()).isTrue();
+        assertThat(args.cardinality()).isZero();
+        assertThat(args.has('x')).isFalse();
+        assertThat(args.getString('x')).isEmpty();
+    }
+
+    @DisplayName("String 값이 있는 경우")
+    @Test
+    void simpleStringPresent() throws Exception {
+        //given
+        String schema = "x*";
+        String[] arguments = new String[]{"-x", "param"};
 
         //when
         Args args = new Args(schema, arguments);
@@ -116,8 +140,78 @@ class ArgsTest {
         //then
         assertThat(args.isValid()).isTrue();
         assertThat(args.cardinality()).isOne();
-        assertThat(args.getBoolean('x')).isFalse();
-        assertThat(args.getBoolean('y')).isFalse();
-        assertThat(args.getBoolean('z')).isTrue();
+        assertThat(args.has('x')).isTrue();
+        assertThat(args.getString('x')).isEqualTo("param");
+    }
+
+    @DisplayName("String 값이 여러 개 있는 경우")
+    @Test
+    void simpleStringMultiplePresent() throws Exception {
+        //given
+        String schema = "x*,y*,z*";
+        String[] arguments = new String[]{"-x", "param1", "-y", "param2"};
+
+        //when
+        Args args = new Args(schema, arguments);
+
+        //then
+        assertThat(args.isValid()).isTrue();
+        assertThat(args.cardinality()).isEqualTo(2);
+        assertThat(args.has('x')).isTrue();
+        assertThat(args.has('y')).isTrue();
+        assertThat(args.has('z')).isFalse();
+        assertThat(args.getString('x')).isEqualTo("param1");
+        assertThat(args.getString('y')).isEqualTo("param2");
+        assertThat(args.getString('z')).isEmpty();
+    }
+
+    @DisplayName("String Argument 값이 없는 경우")
+    @Test
+    void missingStringArgument() throws Exception {
+        //given
+        String schema = "x*";
+        String[] arguments = new String[]{"-x"}; // missing
+
+        //when
+        Args args = new Args(schema, arguments);
+
+        //then
+        assertThat(args.isValid()).isFalse();
+        assertThat(args.cardinality()).isOne();
+        assertThat(args.has('x')).isTrue();
+        assertThat(args.getString('x')).isEmpty();
+        assertThat(args.errorMessage()).isEqualTo("Could not find string parameter for -x.");
+    }
+
+    @DisplayName("Schema의 ElementId가 문자가 아니면 ParseException이 발생한다")
+    @Test
+    void throwParseExceptionWhenInvalidSchemaElementId() {
+        //given
+        String containsNonLetterElementId = "l,d*,#";
+        String[] arguments = new String[0];
+
+        //when
+        Exception exception = catchException(() -> new Args(containsNonLetterElementId, arguments));
+
+        //then
+        assertThat(exception)
+                .isInstanceOf(ParseException.class)
+                .hasMessage("Bad character: # in Args format: l,d*,#");
+    }
+
+    @DisplayName("유효한 args에서 errorMessage를 조회할 경우 Excpetion이 발생한다")
+    @Test
+    void errorMessageThrowExceptionWhenErrorCodeIsOK() throws Exception {
+        //given
+        Args args = new Args("x", new String[]{"-x"});
+        assertThat(args.isValid()).isTrue();
+
+        //when
+        Exception exception = catchException(args::errorMessage);
+
+        //then
+        assertThat(exception)
+                .isInstanceOf(Exception.class)
+                .hasMessage("TILT: Should not get here.");
     }
 }
