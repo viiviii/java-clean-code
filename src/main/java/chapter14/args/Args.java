f4d2ed1695dcef61dbeb6f09e1c67afe95328284
viiviii <viiiix.viiviii@gmail.com>
2022-05-12 09:18:33 +0900

    14. stringArgs, intArgs Map도 모두 제거

--- src/main/java/chapter14/args/Args.java
+++ src/main/java/chapter14/args/Args.java
@@ -8,8 +8,6 @@ public class Args {
     private String[] args;
     private boolean valid = true;
     private Set<Character> unexpectedArguments = new TreeSet<>();
-    private Map<Character, ArgumentMarshaler> stringArgs = new HashMap<>();
-    private Map<Character, ArgumentMarshaler> intArgs = new HashMap<>();
     private Map<Character, ArgumentMarshaler> marshalers = new HashMap<>();
     private Set<Character> argsFound = new HashSet<>();
     private int currentArgument;
@@ -79,13 +77,11 @@ public class Args {
 
     private void parseIntegerSchemaElement(char elementId) {
         ArgumentMarshaler m = new IntegerArgumentMarshaler();
-        intArgs.put(elementId, m);
         marshalers.put(elementId, m);
     }
 
     private void parseStringSchemaElement(char elementId) {
         ArgumentMarshaler m = new StringArgumentMarshaler();
-        stringArgs.put(elementId, m);
         marshalers.put(elementId, m);
     }
 
@@ -224,13 +220,21 @@ public class Args {
     }
 
     public String getString(char arg) {
-        ArgumentMarshaler am = stringArgs.get(arg);
-        return am == null ? "" : (String) am.get();
+        ArgumentMarshaler am = marshalers.get(arg);
+        try {
+            return am == null ? "" : (String) am.get();
+        } catch (ClassCastException e) {
+            return "";
+        }
     }
 
     public int getInt(char arg) {
-        ArgumentMarshaler am = intArgs.get(arg);
-        return am == null ? 0 : (Integer) am.get();
+        ArgumentMarshaler am = marshalers.get(arg);
+        try {
+            return am == null ? 0 : (Integer) am.get();
+        } catch (ClassCastException e) {
+            return 0;
+        }
     }
 
     public boolean getBoolean(char arg) {
